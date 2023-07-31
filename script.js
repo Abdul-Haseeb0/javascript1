f0db5607010451e9cda1d8f61f9116dd12ac0273
Abdul-Haseeb0 <abdulhaseeb0117@gmail.com>
2023-07-31 09:58:09 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+var x = prompt("enter your number:")
+if ( x % 2 == 0 )
+{ alert("Even Number")}
+
+else{
+    alert ("Odd Number")
+}
+
+
+// var x = prompt ("enter your number:")
+
+// if ( x < 0 )
+// {alert ("Negative")}
+
+//  else if ( x > 0 )
+// {alert ("Positive")}
+// else{
+//     alert("Neutral")
+// }
+
+
+// var x = prompt ("Enter First Number")
+// var y = prompt ("Enter Second Number")
+
+// if ( x === y )
+// { alert("Both numbers are equal.")}
+// else if ( x < y )
+// {alert("The first number is smaller than  second number.")}
+// else {
+//     alert("The first number is greater than  second number.")}
